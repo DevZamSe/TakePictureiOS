a8ea141cad6ffdba8792097718bb7d97c5fb551f
devzamse <nilovila@e-quipu.pe>
2020-02-20 18:07:58 -0500

    first version

---
+++ TakeaPicture/ViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  TakeaPicture
+//
+//  Created by Nilo Vila on 2/20/20.
+//  Copyright © 2020 Nilo Vila. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cameraView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
+    var backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if #available(iOS 10.2, *){
+            let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice!)
+                captureSession = AVCaptureSession()
+                captureSession?.addInput(input)
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                videoPreviewLayer?.frame = view.layer.bounds
+                cameraView.layer.addSublayer(videoPreviewLayer!)
+                captureSession?.startRunning()
+            } catch {
+                print("Error no podemos acceder a la camara")
+            }
+        }
+    }
+
+    @IBAction func imageCapture(_ sender: Any) {
+    }
+    
+    func switchToFrontCamera() {
+        if frontCamera?.isConnected == true {
+            captureSession?.stopRunning()
+            let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice!)
+                captureSession = AVCaptureSession()
+                captureSession?.addInput(input)
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                videoPreviewLayer?.frame = view.layer.bounds
+                cameraView.layer.addSublayer(videoPreviewLayer!)
+                captureSession?.startRunning()
+            } catch {
+                print("Error no se puede acceder a la camara trasera")
+            }
+        }
+    }
+    
+    func switchToBackCamera() {
+        if backCamera?.isConnected == true {
+            captureSession?.stopRunning()
+            let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice!)
+                captureSession = AVCaptureSession()
+                captureSession?.addInput(input)
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                videoPreviewLayer?.frame = view.layer.bounds
+                cameraView.layer.addSublayer(videoPreviewLayer!)
+                captureSession?.startRunning()
+            } catch {
+                print("Error no se puede acceder a la camara delantera")
+            }
+        }
+        
+    }
+    
+    @IBAction func rotateCamera(_ sender: Any) {
+        guard let currentCameraInput : AVCaptureInput = captureSession?.inputs.first
+        else {
+            return
+        }
+        if let input = currentCameraInput as? AVCaptureDeviceInput {
+            if input.device.position == .back {
+                switchToFrontCamera()
+            }
+            if input.device.position == .front {
+                switchToBackCamera()
+            }
+        }
+    }
+    
+    @IBAction func flashButton(_ sender: Any) {
+    }
+    
+}
+
